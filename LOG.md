8564ab0a836f351ef36034d506416badac8787c1
select-this <darrenferrie@gmail.com>
2018-01-11 21:34:29 +0000

    Day 2 completed

--- LOG.md
+++ LOG.md
@@ -1,5 +1,24 @@
 # 100 Days of Code 2018 - Log
----
-##### Day 1: 10/01/2017
-**Progress**: Set up project, build and unit test environment.
-**Thoughts:** I'd forgotten how much fun it is to spend an age tweaking your IDE settings to be *just* right.
+
+##### Day 1: 10/01/2018
+
+
+**Progress:**
+- Set up project, build and unit test environment.
+
+**Thoughts:**
+- I'd forgotten how much fun it is to spend an age tweaking your IDE settings to be *just* right
+
+##### Day 2: 11/01/2018
+
+**Progress:**
+- Corrected dates (!) and formatting in the markdown files
+- Updated project to use C++17 rather than C++14
+- Added SDL2 dependency
+- Added basic window with two rectangles to represent a depleting bar
+
+**Thoughts:**
+- Not as much progress as I'd like, but at least it's a start!
+- I've decided this little project will be somewhat a test bed for  getting used to Data Oriented Design principles. To that end, I set up a quick environment in SDL2 since it's something I'm pretty familiar with and doesn't introduce dependency hell, and added a couple of rectangles for my first test.
+- The tests *should* be relevant to things that I am either currently working on, or plan to work on in the future, so nothing should be wasted effort.
+- Not sure exactly when I got into the habit of placing comment-based separators in my source files, but I find they neatly compartmentalise things for easier reading
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//----------------------------------------------------------------------------------------------------------------------
 //
-// Copyright (c) 2018 Darren Paul Ferrie
+// Copyright(c) 2018 Darren Paul Ferrie
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
-// documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+// documentation files(the "Software"), to deal in the Software without restriction, including without limitation the
 // rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
 // permit persons to whom the Software is furnished to do so, subject to the following conditions:
 //
@@ -15,11 +15,124 @@
 // COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
 // OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//----------------------------------------------------------------------------------------------------------------------
 
-int main ()
+#include <cmath>
+#include <cstdint>
+#include <exception>
+#include <iostream>
+#include <memory>
+#include <SDL2/SDL.h>
+
+//----------------------------------------------------------------------------------------------------------------------
+
+int main()
 {
+    try
+    {
+        if(SDL_Init(SDL_INIT_VIDEO) != 0 ||
+            SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3) != 0 ||
+            SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3) != 0 ||
+            SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1) != 0 ||
+            SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24) != 0)
+        {
+            throw std::runtime_error(SDL_GetError());
+        }
+
+        constexpr int WINDOW_WIDTH = 1024;
+        constexpr int WINDOW_HEIGHT = 768;
+        constexpr int WINDOW_HPOS = SDL_WINDOWPOS_UNDEFINED;
+        constexpr int WINDOW_VPOS = SDL_WINDOWPOS_UNDEFINED;
+        constexpr int WINDOW_FLAGS = SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN;
+        constexpr const char* WINDOW_TITLE = "100 Days of Code 2018";
+
+        std::unique_ptr<SDL_Window, void(*)(SDL_Window*)> window(SDL_CreateWindow(
+            WINDOW_TITLE, WINDOW_HPOS, WINDOW_VPOS, WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_FLAGS), SDL_DestroyWindow);
+
+        if(!window)
+        {
+            throw std::runtime_error(SDL_GetError());
+        }
+
+        std::unique_ptr<void, void(*)(SDL_GLContext)> context(SDL_GL_CreateContext(window.get()), SDL_GL_DeleteContext);
+
+        if(!context)
+        {
+            throw std::runtime_error(SDL_GetError());
+        }
+
+        std::unique_ptr<SDL_Renderer, void(*)(SDL_Renderer*)> renderer(
+            SDL_CreateRenderer(window.get(), -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC),
+            SDL_DestroyRenderer);
+
+        if(!renderer)
+        {
+            throw std::runtime_error(SDL_GetError());
+        }
+
+        SDL_Event event;
+
+        bool quit = false;
+
+        while(!quit)
+        {
+            while(SDL_PollEvent(&event))
+            {
+                switch(event.type)
+                {
+                    case SDL_QUIT:
+                        quit = true;
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+
+            SDL_SetRenderDrawColor(renderer.get(), 0, 0, 0, 255);
+
+            SDL_RenderClear(renderer.get());
+
+            int renderer_width = 0;
+            int renderer_height = 0;
+
+            SDL_GetRendererOutputSize(renderer.get(), &renderer_width, &renderer_height);
+
+            SDL_Rect outer_rect
+            {
+                static_cast<int>(std::floor(renderer_width / 2 - 200 + 0.5)),
+                static_cast<int>(std::floor(renderer_height / 2 - 70 + 0.5)),
+                400,
+                140
+            };
+
+            SDL_SetRenderDrawColor(renderer.get(), 50, 100, 200, 255);
+
+            SDL_RenderFillRect(renderer.get(), &outer_rect);
+
+            SDL_Rect inner_rect
+            {
+                static_cast<int>(std::floor(renderer_width / 2 - 190 + 0.5)),
+                static_cast<int>(std::floor(renderer_height / 2 - 60 + 0.5)),
+                380,
+                120
+            };
+
+            SDL_SetRenderDrawColor(renderer.get(), 20, 70, 170, 255);
+
+            SDL_RenderFillRect(renderer.get(), &inner_rect);
+
+            SDL_RenderPresent(renderer.get());
+        }
+    }
+    catch(std::runtime_error& exception)
+    {
+        std::cout << "Oh no, something bad happened! Exception: " << exception.what() << std::endl;
+    }
+
+    SDL_Quit();
+
     return 0;
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//----------------------------------------------------------------------------------------------------------------------
